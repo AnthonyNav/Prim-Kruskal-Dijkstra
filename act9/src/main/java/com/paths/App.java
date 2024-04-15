3ab1fe21fbe708a3c3a8a5e391d4c7b6ee092544
AnthonyNav <anthonydevxp@gmail.com>
2024-04-14 22:35:25 -0600

    project base done

--- act9/src/main/java/com/paths/App.java
+++ act9/src/main/java/com/paths/App.java
@@ -1,12 +1,143 @@
 package com.paths;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+    private static ListaLSimple adjacent; // listas de adyacencias
+    private static HashMap<String,String> father; // Arreglo padre
+    private static HashMap<String, Integer> distance; // Arreglo de distancia
+    private static HashMap<String, Boolean> visited; // Arreglo de visitado
+    private static String[] sequence; // Orden
     public static void main(String[] args) {
-        System.out.println( "Hello World!" );
+        
+        Scanner scan = new Scanner(System.in);
+        boolean salir = false;
+        boolean salirMenu;
+        int op;
+
+        while (!salir) {
+            boolean cargaCompleta = false;
+            // Cargamos los datos del grafo
+            while (!cargaCompleta) {
+                System.out.println("\n\n");
+                System.out.println("Ingrese el grafo que desea (1-8):");
+                cargaCompleta = cargarGrafo(scan.nextInt());
+            }
+            salirMenu = false;
+            while (!salirMenu) {
+                System.out.println("\n\nQue Algoritmo desea?\n1)Arbol de expancion minimo (Prim)\n2)Arbol de expancion minimo (Kruskar)\n3)Camino Minimo (Dijkstra)\n4)Salir");
+                op = scan.nextInt();
+                switch (op) {
+                    case 1:
+                        // Algoritmo de prim 
+                        System.out.println("Prim");
+                        break;
+                    case 2:
+                        // Algoritmo de kruskal
+                        System.out.println("Kruskal");
+                        break;
+                    case 3:
+                        System.out.println("dijkstra");
+                        // Algoritmo de dijkstra
+                        break;
+                    case 4:
+                        salirMenu = true;
+                        break;
+                    default:
+                    System.out.println("Opcion no valida");
+                        break;
+                }
+                
+            }
+            System.out.println("\n\nDesea probar con otro grafo?\n1)Si\n2)No");
+            salir = (scan.nextInt() == 2)? true:salir;   
+        }
+        scan.close();
+
+    }
+
+    // Procedimiento para cargar el contenido de los grafos
+    public static boolean cargarGrafo(int gn){
+        String direccion = "";
+        switch (gn) {
+            case 1:
+                direccion = "act9/src/main/java/com/paths/Grafos/G1.txt";
+                break;
+            case 2:
+                direccion = "C:/Users/Christian/Documents/Proyectos/BusquedaEnGrafos/grafos/src/main/java/com/busqueda/Grafos/G2.txt";
+                break;
+            case 3:
+                direccion = "C:/Users/Christian/Documents/Proyectos/BusquedaEnGrafos/grafos/src/main/java/com/busqueda/Grafos/G3.txt";
+                break;
+            case 4:
+                direccion = "C:/Users/Christian/Documents/Proyectos/BusquedaEnGrafos/grafos/src/main/java/com/busqueda/Grafos/G4.txt";
+                break;
+            case 5:
+                direccion = "C:/Users/Christian/Documents/Proyectos/BusquedaEnGrafos/grafos/src/main/java/com/busqueda/Grafos/G5.txt";
+                break;
+            case 6:
+                direccion = "C:/Users/Christian/Documents/Proyectos/BusquedaEnGrafos/grafos/src/main/java/com/busqueda/Grafos/G6.txt";
+                break;
+            case 7:
+                direccion = "C:/Users/Christian/Documents/Proyectos/BusquedaEnGrafos/grafos/src/main/java/com/busqueda/Grafos/G7.txt";
+                break;
+            case 8:
+                direccion = "act9/src/main/java/com/paths/Grafos/G8.txt";
+                break;
+            default:
+                System.out.println("Opcion no valida");
+                return false;
+        }
+
+        try{
+            File archivo = new File(direccion);
+            // Crear un objeto Scanner para leer el archivo
+            Scanner scanner = new Scanner(archivo);
+            // Declaracion de variables
+            String linea;
+            String[] str, vert;
+            adjacent = new ListaLSimple();
+            ListaLSimple aux = adjacent;
+            father = new HashMap<>();
+            visited = new HashMap<>();
+            distance = new HashMap<>();
+
+            // Leer el archivo línea por línea
+            while (scanner.hasNextLine()) {
+                linea = scanner.nextLine();           
+                // System.out.println(linea);                     
+                str = linea.split(":");                
+                aux.setVertex(String.format("%-2s", str[0].replaceAll(" ", "")));
+                // Inicializacion de arreglos
+                father.put(str[0].replaceAll(" ", ""), "-");
+                distance.put(str[0].replaceAll(" ", ""), Integer.MAX_VALUE);
+                visited.put(str[0].replaceAll(" ", ""), false);
+                // Inicializacion de la lista adyacente
+                str = str[1].split(" ");
+                for (String s : str) {
+                    // Ingresando datos a lalista de adyacencia
+                    vert = s.split("-");
+                    aux.insert(vert[0], Integer.parseInt(vert[1]));
+                }
+                
+
+                System.out.println("Lista "+aux.getVertex()+": " + aux); // Para ver lo como esta leyendo el archivo
+                aux.setNextList(new ListaLSimple());
+                aux = aux.getNextList();
+            }
+            // Cerrar el Scanner después de leer todas las líneas
+            scanner.close();
+            sequence = new String[father.size()];
+            // Captura de los vertices en formato String en un arreglo
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
     }
 }
--- act9/src/main/java/com/paths/ListaLSimple.java
+++ act9/src/main/java/com/paths/ListaLSimple.java
@@ -1,11 +1,13 @@
 package com.paths;
 
 public class ListaLSimple {
-
+    private String vertex; // Id del vertice 
     private Node start; // Identifica el nodo inicial
     private ListaLSimple nextList; // Enlaza la siguiente lista de adyacencia
+    private int nodes; // Representa la cantidad de adyacencias que tiene
     
     public ListaLSimple() {
+        nodes = 0;
         start = null;
         nextList = null;
     }
@@ -25,7 +27,12 @@ public class ListaLSimple {
     public void setNextList(ListaLSimple nextList) {
         this.nextList = nextList;
     }
-
+    public String getVertex() {
+        return vertex;
+    }
+    public void setVertex(String vertex) {
+        this.vertex = vertex;
+    }
     // Metodos de insercion
     public void insert(String vertex, int weight){
         if (this.estaVacia()) {
@@ -37,6 +44,7 @@ public class ListaLSimple {
             }
             auxNodo.setNext(new Node(vertex, weight)); // Inserta la referencia al nuevo nodo
         }
+        nodes++;
     }
 
     public Node getNodeX(String vertex){ // Nos retorna el nodo con el vertice ingresado por parametros
@@ -47,14 +55,43 @@ public class ListaLSimple {
         return auxNodo; // Si existe el vertice, retorna el nodo, sino retornara null
     }
 
+    public int[] getWeights(){
+        int i=0;
+        int[] n = new int[nodes];
+        Node auxNode = this.start;
+        while (auxNode != null) {
+            n[i++]=auxNode.getWeight();
+            auxNode = auxNode.getNext();
+
+        }
+        return n;
+    }
+
+    public String[] getVertices(){
+        int i=0;
+        String[] n = new String[nodes];
+        Node auxNode = this.start;
+        while (auxNode != null) {
+            n[i++]=auxNode.getVertex();
+            auxNode = auxNode.getNext();
+        }
+        return n;
+    }
     
  
     public boolean estaVacia(){
         return start==null;
     }
-
-    
-
-    
-
+    @Override
+    public String toString(){ // Asume que no esta vacia la lista
+        String output = "";
+        String aux = "";
+        Node auxNodo = this.start;
+        while (auxNodo != null) { // Recorre la lista
+            aux = String.format("%-3s-%3d", auxNodo.getVertex(), auxNodo.getWeight());
+            output = output + " | "+ aux; // concatena
+            auxNodo = auxNodo.getNext();
+        }
+        return output+" |"; // Retorna nuestra cadena que representa los elementos existentes
+    }
 }
